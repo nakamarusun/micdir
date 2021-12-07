72341aac84d6c66d6a4d92d75a1a9875534b9330
Jason Christian H <nakamarujc@gmail.com>
2021-12-07 16:05:12 +0700

    Updated test.cpp to support multiple detections

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #define TEST
 #include <cmath>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Create functions and definitions to replace the ones from the arduino
 // library
@@ -19,10 +21,27 @@ uint32_t micros() {
 }
 
 #define SOUND_TIME 100000 // Sound duration
+#define TIME_OFFSET 7 // Offset to be added to the dets array
+// Whether the mic is already triggered once, next ones is gonna be noise.
+uint8_t trigd[] = {0, 0, 0};
+
 // Time of microphone triggers (Ordered per pin)
-uint32_t dets[] = {4380077, 4380000, 4380146};
+uint32_t mul_i = 0;
+uint32_t dets_multi[][3] = {
+  {9 + TIME_OFFSET, 130 + TIME_OFFSET, 0 + TIME_OFFSET},
+  {1000 + 116 + TIME_OFFSET, 1000 + 0 + TIME_OFFSET, 1000 + 19 + TIME_OFFSET},
+};
+
 uint8_t digitalRead(uint8_t pin) {
-  return _curtime > dets[pin] && _curtime < (dets[pin]  + SOUND_TIME);
+  if (_curtime > dets_multi[mul_i][pin] && _curtime < (dets_multi[mul_i][pin] + SOUND_TIME)) {
+    if (trigd[pin]) {
+      return (rand() % 100) < 10; // Simulate noise
+    } else {
+      trigd[pin] = 1;
+      return true;
+    }
+  }
+  return false;
 }
 
 // Load the microcontroller file
@@ -43,11 +62,23 @@ void assert_equal(const char test[], T a, T b) {
 #define STOP_AFTER 5000000 // Stop the program after x seconds has passed (us)
 
 
+// Reset sensors
+void reset_test_sensor() {
+  mul_i++;
+  for (int i = 0; i < sizeof(trigd)/sizeof(trigd[0]); i++) trigd[0] = 0;
+}
+
 // When the sensors have determined a direction
 void on_detection(double direction) {
   printf("Detected direction: %f\n", direction * 180 / PI);
-  printf("Test done, simulated time: %dus", _curtime);
-  exit(0);
+  
+  // If all detection is not done, reset sensor and continue
+  if ((mul_i + 1) < (sizeof(dets_multi)/sizeof(dets_multi[0]))) {
+    reset_test_sensor();
+  } else {
+    printf("Test done, simulated time: %dus", _curtime);
+    exit(0);
+  }
 }
 
 // Main loop
@@ -63,6 +94,7 @@ bool test_looper() {
 }
 
 int main() {
+  srand(time(NULL));
   ASSERT_EQUAL("Sanity check 1=1", 1, 1);
 
   // Do loops
